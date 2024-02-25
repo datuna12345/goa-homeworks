9170a0ff4e26aa8ad0dcd3f01c05ccbefd9a82d0
datuna12345 <rocknwings3.6@gmail.com>
2024-02-25 15:43:13 +0400

    third commit

---
+++ day 04/homework/homework4.py
@@ -0,0 +1,51 @@
+#მიმატება
+
+print(10 + 15)
+print(13 + 12)
+print(25 + 14)
+print(55 + 60)
+print(100 + 200)
+print(150 + 20)
+print(500 + 1000)
+print(400 + 350)
+print(200 + 400)
+print(25000 + 1500)
+
+#გამოკლება
+
+print(90 - 30)
+print(7000 - 2000)
+print(10 - 5)
+print(50 - 39)
+print(70 - 65)
+print(25000 - 999)
+print(60 - 30)
+print(2000 - 3000)
+print(5 - 5)
+print(20 - 15)
+
+#გამრავლება
+
+print(33 * 44)
+print(44 * 78)
+print(68 * 25)
+print(99 * 11)
+print(999 * 777)
+print(888 * 678)
+print(60 * 30)
+print(11 * 22)
+print(77 * 23)
+print(99 * 99)
+
+#გაყოფა
+
+print(15 // 2)
+print(60 // 30)
+print(100 // 5)
+print(66 // 33)
+print(50 // 25)
+print(40 // 2)
+print(120 // 20)
+print(5 // 5)
+print( 60 // 2)
+print(2024 // 1121)
