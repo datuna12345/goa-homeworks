ba885b61a72276c0b890d8ed77e62a3af8d3cef5
datuna12345 <rocknwings3.6@gmail.com>
2024-03-22 16:39:53 +0400

    seventh commit

---
+++ day 07/homework/homework7.1.py
@@ -0,0 +1,76 @@
+#დავალება N2
+
+
+
+num1 = int(input("please enter any number you want "))
+num2 = int(input("please enter any number you want "))
+num3 = int(input("please enter any number you want "))
+num4 = int(input("please enter any number you want "))
+num5 = int(input("please enter any number you want "))
+
+print(num1 + num2)
+print(num1 + num3)
+print(num1 + num4)
+print(num1 + num5)
+print(num2 + num3)
+print(num2 + num4)
+print(num2 + num5)
+print(num3 + num4)
+print(num3 + num5)
+print(num4 + num5)
+
+
+
+print(num1 - num2)
+print(num1 - num3)
+print(num1 - num4)
+print(num1 - num5)
+print(num2 - num3)
+print(num2 - num4)
+print(num2 - num5)
+print(num3 - num4)
+print(num3 - num5)
+print(num4 - num5)
+
+
+
+print(num1 * num2)
+print(num1 * num3)
+print(num1 * num4)
+print(num1 * num5)
+print(num2 * num3)
+print(num2 * num4)
+print(num2 * num5)
+print(num3 * num4)
+print(num3 * num5)
+print(num4 * num5)
+
+
+
+print(num1 / num2)
+print(num1 / num3)
+print(num1 / num4)
+print(num1 / num5)
+print(num2 / num3)
+print(num2 / num4)
+print(num2 / num5)
+print(num3 / num4)
+print(num3 / num5)
+print(num4 / num5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day 07/homework/homework7.py
@@ -0,0 +1,73 @@
+#დავალება N1
+age = 12
+name = ("dato")
+bank_account = 21.5
+
+
+
+
+print(type (age))
+print(type (name))
+print(type (bank_account))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
